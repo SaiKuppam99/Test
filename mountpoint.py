458b29cf0a8d4b016e5f9353e42f7c78d15fa079
shamuzib <shaiksami179@gmail.com>
2023-03-14 20:36:12 +0000

    Update mountpoint.py

--- mountpoint.py
+++ mountpoint.py
@@ -1,14 +1,38 @@
 import hvac
 
-def list_paths_and_secrets(mount_point, path):
-    # Create a Vault client object
+def list_secret_engine_paths(mount_point, path):
+    """
+    Lists the available paths in a Vault secret engine.
+    """
     client = hvac.Client(url='https://vault.example.com', token='<your-token>')
-
-    # List the available paths in the secret engine
     list_response = client.secrets.kv.v2.list_secrets(mount_path=mount_point, path=path)
     keys = list_response['data']['keys']
+    paths = []
+    for key in keys:
+        if key.endswith('/'):
+            paths.append(key)
+    return paths
 
-    # Separate paths and secrets
+def list_secret_engine_secrets(mount_point, path):
+    """
+    Lists the available secrets in a Vault secret engine.
+    """
+    client = hvac.Client(url='https://vault.example.com', token='<your-token>')
+    list_response = client.secrets.kv.v2.list_secrets(mount_path=mount_point, path=path)
+    keys = list_response['data']['keys']
+    secrets = []
+    for key in keys:
+        if not key.endswith('/'):
+            secrets.append(key)
+    return secrets
+
+def list_secret_engine_paths_and_secrets(mount_point, path):
+    """
+    Lists the available paths and secrets in a Vault secret engine.
+    """
+    client = hvac.Client(url='https://vault.example.com', token='<your-token>')
+    list_response = client.secrets.kv.v2.list_secrets(mount_path=mount_point, path=path)
+    keys = list_response['data']['keys']
     paths = []
     secrets = []
     for key in keys:
@@ -16,21 +40,10 @@ def list_paths_and_secrets(mount_point, path):
             paths.append(key)
         else:
             secrets.append(key)
-
-    # Check if there are any secrets under each path
     for p in paths:
         list_response = client.secrets.kv.v2.list_secrets(mount_path=mount_point, path=path + p)
         if list_response['data']['keys']:
             secrets.append(p)
         else:
             paths.append(p)
-
-    # Return the paths and secrets as a tuple
     return paths, secrets
-
-# Example usage
-mount_point = 'TASK'
-path = 'hvac'
-paths, secrets = list_paths_and_secrets(mount_point, path)
-print(f"Paths: {paths}")
-print(f"Secrets: {secrets}")
