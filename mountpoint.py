f8bab0ec95a98826b164308a5c9790a215802b8f
shamuzib <shaiksami179@gmail.com>
2023-03-15 14:10:36 +0000

    Update mountpoint.py

--- mountpoint.py
+++ mountpoint.py
@@ -1,67 +1,28 @@
 import hvac
 
-def list_secret_engine_paths(mount_point, path):
-    """
-    Lists the available paths in a Vault secret engine.
-    """
-    client = hvac.Client(url='https://vault.example.com', token='<your-token>')
-    list_response = client.secrets.kv.v2.list_secrets(mount_path=mount_point, path=path)
-    keys = list_response['data']['keys']
-    paths = []
-    for key in keys:
-        if key.endswith('/'):
-            paths.append(key)
-    return paths
+def connect_to_vault(url, token):
+    client = hvac.Client(url=url, token=token)
+    return client
 
-def list_secret_engine_secrets(mount_point, path):
-    """
-    Lists the available secrets in a Vault secret engine.
-    """
-    client = hvac.Client(url='https://vault.example.com', token='<your-token>')
-    list_response = client.secrets.kv.v2.list_secrets(mount_path=mount_point, path=path)
-    keys = list_response['data']['keys']
-    secrets = []
-    for key in keys:
-        if not key.endswith('/'):
-            secrets.append(key)
-    return secrets
-
-def list_secret_engine_paths_and_secrets(mount_point, path):
-    """
-    Lists the available paths and secrets in a Vault secret engine.
-    """
-    client = hvac.Client(url='https://vault.example.com', token='<your-token>')
-    list_response = client.secrets.kv.v2.list_secrets(mount_path=mount_point, path=path)
-    keys = list_response['data']['keys']
+def get_paths_and_secrets(client, mount_point):
     paths = []
     secrets = []
-    for key in keys:
+    list_response = client.secrets.kv.v2.list_secrets(
+        mount_path=mount_point
+    )
+    for key in list_response['data']['keys']:
         if key.endswith('/'):
             paths.append(key)
         else:
             secrets.append(key)
-    for p in paths:
-        list_response = client.secrets.kv.v2.list_secrets(mount_path=mount_point, path=path + p)
-        if list_response['data']['keys']:
-            secrets.append(p)
-        else:
-            paths.append(p)
     return paths, secrets
 
-from lib import vault
-
-mount_point = 'TASK'
-path = 'hvac'
-
-# List the available paths in the secret engine
-paths = vault.list_secret_engine_paths(mount_point, path)
-print(f"Paths: {paths}")
-
-# List the available secrets in the secret engine
-secrets = vault.list_secret_engine_secrets(mount_point, path)
-print(f"Secrets: {secrets}")
-
-# List the available paths and secrets in the secret engine
-paths, secrets = vault.list_secret_engine_paths_and_secrets(mount_point, path)
-print(f"Paths: {paths}")
-print(f"Secrets: {secrets}")
+def check_if_secret_exists(client, mount_point, secret_path):
+    response = client.secrets.kv.v2.read_secret_version(
+        mount_point=mount_point,
+        path=secret_path
+    )
+    if 'data' in response:
+        return True
+    else:
+        return False
