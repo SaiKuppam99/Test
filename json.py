2159cc40b35a0ddf4f19aa9afbcd9087dc03ba0b
shamuzib <shaiksami179@gmail.com>
2023-03-18 10:15:30 +0000

    Update json.py

--- json.py
+++ json.py
@@ -1,23 +1,36 @@
+import os
 import boto3
 import pandas as pd
 import tempfile
 
-# Connect to the S3 service
-s3 = boto3.client('s3')
+def xls_to_json(bucket_name, file_path, columns):
+    # Set the AWS profile
+    os.environ['AWS_PROFILE'] = 'my-profile'
 
-# Download the XLS file to a temporary file
-with tempfile.TemporaryFile() as tmp_file:
-    s3.download_file('your-bucket-name', 'path/to/your-file.xls', tmp_file)
+    # Connect to the S3 service
+    s3 = boto3.client('s3')
 
-    # Rewind the file pointer to the beginning of the file
-    tmp_file.seek(0)
+    # Download the XLS file to a temporary file
+    with tempfile.TemporaryFile() as tmp_file:
+        s3.download_file(bucket_name, file_path, tmp_file)
 
-    # Load the XLS file into a pandas DataFrame
-    df = pd.read_excel(tmp_file)
+        # Rewind the file pointer to the beginning of the file
+        tmp_file.seek(0)
 
-# Convert the DataFrame to a JSON string
-json_str = df.to_json(orient='records')
+        # Load only the selected columns into a pandas DataFrame
+        df = pd.read_excel(tmp_file, usecols=columns)
 
-# Write the JSON string to a file
-with open('path/to/output.json', 'w') as f:
-    f.write(json_str)
+    # Check for empty values in the User Account and Password columns
+    mask = df[['User Account', 'Password']].isnull().any(axis=1)
+    if mask.any():
+        # Empty values found
+        print('Empty values found in the User Account or Password columns:')
+        print(df[mask])
+    else:
+        # No empty values found
+        print('No empty values found in the User Account or Password columns')
+
+    # Convert the DataFrame to a JSON string
+    json_str = df.to_json(orient='records')
+    
+    return json_str
