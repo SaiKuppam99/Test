aac2a1ab08551cc2249f4ea5943b1e36b120f78f
shamuzib <shaiksami179@gmail.com>
2023-08-03 11:50:31 +0100

    Update check_status.py
    Signed-off-by: shamuzib <shaiksami179@gmail.com>

--- functions/check_status.py
+++ functions/check_status.py
@@ -1,3 +1,104 @@
 def check_vault_seal_status(client):
     seal_status = client.sys.read_seal_status()
     return seal_status['sealed']
+
+
+provider "aws" {
+  region = "eu-west-2"  # Replace with your desired AWS region
+}
+
+variable "rds_instance_id" {
+  description = "The ID of the RDS instance to monitor"
+}
+
+locals {
+  thresholds = {
+    missing_75 = 0.75,
+    critical_90 = 0.9
+  }
+
+  include_metrics = [
+    {
+      name       = "ACUUtilization"
+      statistic  = "Average"
+      namespace  = "AWS/RDS"
+      dimensions = { DBInstanceIdentifier = var.rds_instance_id }
+      comparison_operator = "GreaterThanOrEqualToThreshold"  # Use GreaterThanOrEqualToThreshold for ACUUtilization and CPUUtilization
+    },
+    {
+      name       = "ServerlessDatabaseCapacity"
+      statistic  = "Maximum"
+      namespace  = "AWS/RDS"
+      dimensions = { DBClusterIdentifier = var.rds_instance_id }
+      comparison_operator = "LessThanOrEqualToThreshold"  # Use LessThanOrEqualToThreshold for ServerlessDatabaseCapacity
+    }
+  ]
+
+  alarms_75 = {
+    for metric in local.include_metrics : metric.name => {
+      alarm_name          = "RDS-${metric.name}-75-Alarm"
+      comparison_operator = metric.comparison_operator
+      evaluation_periods  = 1
+      metric_name         = metric.name
+      namespace           = metric.namespace
+      period              = 60
+      statistic           = metric.statistic
+      dimensions          = metric.dimensions
+      threshold           = local.thresholds.missing_75
+      alarm_description   = "Alarm when ${metric.name} is 75%"
+      alarm_actions       = [aws_sns_topic.example_sns.arn]
+    }
+  }
+
+  alarms_90 = {
+    for metric in local.include_metrics : metric.name => {
+      alarm_name          = "RDS-${metric.name}-90-Alarm"
+      comparison_operator = metric.comparison_operator
+      evaluation_periods  = 1
+      metric_name         = metric.name
+      namespace           = metric.namespace
+      period              = 60
+      statistic           = metric.statistic
+      dimensions          = metric.dimensions
+      threshold           = local.thresholds.critical_90
+      alarm_description   = "Alarm when ${metric.name} is 90%"
+      alarm_actions       = [aws_sns_topic.example_sns.arn]
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_alarms_75" {
+  for_each = local.alarms_75
+
+  alarm_name          = each.value.alarm_name
+  comparison_operator = each.value.comparison_operator
+  evaluation_periods  = each.value.evaluation_periods
+  metric_name         = each.value.metric_name
+  namespace           = each.value.namespace
+  period              = each.value.period
+  statistic           = each.value.statistic
+  dimensions          = each.value.dimensions
+  threshold           = each.value.threshold
+  alarm_description   = each.value.alarm_description
+  alarm_actions       = each.value.alarm_actions
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_alarms_90" {
+  for_each = local.alarms_90
+
+  alarm_name          = each.value.alarm_name
+  comparison_operator = each.value.comparison_operator
+  evaluation_periods  = each.value.evaluation_periods
+  metric_name         = each.value.metric_name
+  namespace           = each.value.namespace
+  period              = each.value.period
+  statistic           = each.value.statistic
+  dimensions          = each.value.dimensions
+  threshold           = each.value.threshold
+  alarm_description   = each.value.alarm_description
+  alarm_actions       = each.value.alarm_actions
+}
+
+resource "aws_sns_topic" "example_sns" {
+  name = "example-sns-topic"
+}
