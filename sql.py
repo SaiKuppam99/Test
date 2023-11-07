582bfa927b55c2fc867a68cfd7b0c0ed4fb9da30
shamuzib <shaiksami179@gmail.com>
2023-11-07 08:34:54 +0000

    Update sql.py
    Signed-off-by: shamuzib <shaiksami179@gmail.com>

--- sql.py
+++ sql.py
@@ -1,26 +1,33 @@
-import json
-
-# Function to generate and execute SQL statements for permissions
-def grant_permissions(users_data):
+def generate_sql_statements(users_data):
+    sql_statements = []
+    
     for user_data in users_data:
         username = user_data['username']
         permissions = user_data['permissions']
         
-        # Iterate through permissions for each database
-        for db_name, db_permissions in permissions.items():
-            # If db_permissions is a list, grant permissions at the database level
-            if isinstance(db_permissions, list):
-                for action in db_permissions:
-                    sql_statement = f"GRANT {action} ON `{db_name}`.* TO '{username}'@'%'"
-                    print(f"Executing SQL: {sql_statement}")
-                    # Execute the SQL statement in your database management system
-            # If db_permissions is a dictionary, handle table-level permissions
-            elif isinstance(db_permissions, dict):
-                for table_name, table_actions in db_permissions.items():
-                    for action in table_actions:
-                        sql_statement = f"GRANT {action} ON `{db_name}`.`{table_name}` TO '{username}'@'%'"
-                        print(f"Executing SQL: {sql_statement}")
-                        # Execute the SQL statement in your database management system
+        for db_name, table_permissions in permissions.items():
+            if db_name == "*":
+                db_name = "*.*"
+            else:
+                db_name = f"`{db_name}`"
+                
+            if isinstance(table_permissions, list):
+                # Permissions for all tables in the database
+                table_permissions = "*"
+            
+            for table_name, actions in table_permissions.items():
+                if table_name == "*":
+                    table_name = "*.*"
+                else:
+                    table_name = f"`{db_name}`.`{table_name}`"
+                
+                for action in actions:
+                    sql_statement = f"GRANT {action} ON {table_name} TO '{username}'@'%';"
+                    sql_statements.append(sql_statement)
+    
+    return sql_statements
 
-# Call the function to grant permissions
-grant_permissions(users_data)
+
+sql_statements = generate_sql_statements(users_data)
+for statement in sql_statements:
+    print(statement)
