acb22a99870a86496342a2f7da6835f51988a742
shamuzib <shaiksami179@gmail.com>
2023-11-07 08:35:55 +0000

    Update sql.py
    Signed-off-by: shamuzib <shaiksami179@gmail.com>

--- sql.py
+++ sql.py
@@ -1,33 +1,40 @@
-def generate_sql_statements(users_data):
+import json
+
+def generate_sql_statements_from_file(file_path):
+    with open(file_path, 'r') as json_file:
+        users_data = json.load(json_file)
+
     sql_statements = []
-    
+
     for user_data in users_data:
         username = user_data['username']
         permissions = user_data['permissions']
-        
+
         for db_name, table_permissions in permissions.items():
             if db_name == "*":
                 db_name = "*.*"
             else:
                 db_name = f"`{db_name}`"
-                
+
             if isinstance(table_permissions, list):
                 # Permissions for all tables in the database
                 table_permissions = "*"
-            
+
             for table_name, actions in table_permissions.items():
                 if table_name == "*":
                     table_name = "*.*"
                 else:
                     table_name = f"`{db_name}`.`{table_name}`"
-                
+
                 for action in actions:
                     sql_statement = f"GRANT {action} ON {table_name} TO '{username}'@'%';"
                     sql_statements.append(sql_statement)
-    
+
     return sql_statements
 
+# Example usage:
+json_file_path = 'path/to/your/json/file.json'
+sql_statements = generate_sql_statements_from_file(json_file_path)
 
-sql_statements = generate_sql_statements(users_data)
 for statement in sql_statements:
     print(statement)
